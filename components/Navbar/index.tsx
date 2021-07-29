541d3f77cd62793b5bbfd3dc1349909a87b28c93
Derek Guenther <dguenther9@gmail.com>
2021-07-28 17:01:00 -0700

    Update flyout colors

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -50,10 +50,10 @@ function NavbarLinks({ className = "", getStartedClassName = "absolute left-0 ri
 
 function NavbarFlyout({ flyoutVisible, closeFlyout }: NavbarFlyoutProps) {
   return (
-    <div className={`absolute h-screen w-screen bg-ifblue text-white font-extended transition-all transform-gpu ${!flyoutVisible ? '-translate-x-full' : ''}`}>
+    <div className={`absolute h-screen w-screen bg-white text-black font-extended transition-all transform-gpu ${!flyoutVisible ? '-translate-x-full' : ''}`}>
       <div className="flex flex-col px-5">
         <div className="flex my-10 justify-between items-center">
-          <div><Logo fill="white" width={190} height={32} /></div>
+          <div><Logo fill="black" width={190} height={32} /></div>
           <button onClick={closeFlyout}><Close /></button>
         </div>
         <NavbarLinks className="leading-relaxed text-4xl" getStartedClicked={() => {}} />
--- components/icons/Close.tsx
+++ components/icons/Close.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-function Close() {
+type Props = { className?: string }
+
+function Close({ className }: Props) {
     return (
-        <svg width="27" height="26" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="#fff" d="M.86 23.334L24.196 0l1.945 1.944L2.805 25.28z"/><path fill="#fff" d="M26.139 23.334L2.804 0 .86 1.945l23.334 23.334z"/></svg>
+        <svg className={className} width="27" height="26" fill="none" xmlns="http://www.w3.org/2000/svg"><path className="fill-current" d="M.86 23.334L24.196 0l1.945 1.944L2.805 25.28z"/><path className="fill-current" d="M26.139 23.334L2.804 0 .86 1.945l23.334 23.334z"/></svg>
     )
 }
 
