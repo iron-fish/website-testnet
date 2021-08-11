900c95ea9ee14976dac478e20b0b7ccd1fdec2b3
Brekk <brekk@brekkbockrath.com>
2021-08-11 09:49:01 -1000

    More automation (#8)
    * add nps and husky hooks
    
    * add documentation to the README
    
    * use local, think global
    
    * add prepare script
    
    * add husky install step to readme

---
+++ package-scripts.js
@@ -0,0 +1,10 @@
+module.exports = {
+  scripts: {
+    dev: 'next dev',
+    build: 'next build',
+    start: 'next start',
+    lint: 'next lint',
+    precommit: 'nps care',
+    care: 'nps build lint',
+  },
+}
