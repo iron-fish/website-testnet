67cabfc183cea9c32796cef1780be64d522268fd
brekk <brekk@brekkbockrath.com>
2021-08-12 20:27:55 -1000

    key test on server

--- apiClient/client.ts
+++ apiClient/client.ts
@@ -7,13 +7,16 @@ import {
 } from './types'
 
 // Environment variables set in Vercel config.
-const API_URL = process.env.API_URL
-const API_KEY = process.env.API_KEY
-const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL
-const NEXT_PUBLIC_API_KEY = process.env.NEXT_PUBLIC_API_KEY
+const SERVER_API_URL = process.env.API_URL
+const SERVER_API_KEY = process.env.API_KEY
+const BROWSER_API_URL = process.env.NEXT_PUBLIC_API_URL
+const BROWSER_API_KEY = process.env.NEXT_PUBLIC_API_KEY
+
+const API_URL = SERVER_API_URL || BROWSER_API_URL
+const API_KEY = SERVER_API_KEY || BROWSER_API_KEY
 
 // eslint-disable-next-line no-console
-console.log({ API_URL, API_KEY, NEXT_PUBLIC_API_URL, NEXT_PUBLIC_API_KEY })
+console.log({ API_URL, API_KEY })
 
 export async function listLeaderboard(): Promise<
   ListLeaderboardResponse | ApiError
