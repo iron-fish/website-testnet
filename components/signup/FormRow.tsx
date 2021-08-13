d6732737de7337d3ffae5f896f73b52d4b99a94e
Brekk <brekk@brekkbockrath.com>
2021-08-13 12:31:38 -1000

    IRO-952 - Alerts CSS (#11)

--- components/signup/FormRow.tsx
+++ components/signup/FormRow.tsx
@@ -6,8 +6,8 @@ interface FormRowProps {
 
 export const FormRow = ({ className = '', valid, children }: FormRowProps) => (
   <div
-    className={`flex flex-col p-2 w-11/12 sm:w-7/12 mb-4 border rounded-md border-solid ${
-      valid ? 'border-black' : 'border-statusred'
+    className={`flex flex-col p-2 mt-3 w-11/12 sm:w-7/12 border rounded-md border-solid ${
+      valid ? 'border-black' : 'border-alertred'
     } ${className}`}
   >
     {children}
