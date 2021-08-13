a27958c8e5141c68c3f06af70b46c4b3c6278098
brekk <brekk@brekkbockrath.com>
2021-08-13 12:28:12 -1000

    change status -> alert and update CSS

--- components/signup/Note.tsx
+++ components/signup/Note.tsx
@@ -6,7 +6,7 @@ interface NoteProps {
 }
 
 export const Note = ({ children, size = 'w-11/12 sm:w-7/12' }: NoteProps) => (
-  <div className={`p-2 text-sm mb-8 bg-statusyellow text-center ${size}`}>
+  <div className={`p-2 text-sm mb-8 bg-alertyellow text-center ${size}`}>
     {children}
   </div>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,10 @@ module.exports = {
         navbar: '0px 4px 12px rgba(0, 0, 0, 0.12)',
       },
       colors: {
-        statusyellow: {
+        alertyellow: {
           DEFAULT: '#FFF18A',
         },
-        statusred: {
+        alertred: {
           DEFAULT: '#F15929',
         },
         ifbeige: {
