63e4357808d40f8ff13bccd88ee58c9b8900d5af
Brekk <brekk@brekkbockrath.com>
2021-08-13 14:34:32 -1000

    IRO-949 Form Field Fixes (#14)

--- components/signup/Note.tsx
+++ components/signup/Note.tsx
@@ -5,8 +5,8 @@ interface NoteProps {
   size?: string
 }
 
-export const Note = ({ children, size = 'w-11/12 sm:w-7/12' }: NoteProps) => (
-  <div className={`p-2 text-sm mb-8 bg-alertyellow text-center ${size}`}>
+export const Note = ({ children, size = 'w-full' }: NoteProps) => (
+  <div className={`p-2 text-sm mb-8 mt-2 bg-alertyellow text-center ${size}`}>
     {children}
   </div>
 )
