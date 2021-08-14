00f1fac95e83c74e32c6d723170ab576ecf09446
Brekk <brekk@brekkbockrath.com>
2021-08-13 15:00:21 -1000

    IRO-950 login-link-padding (#15)

--- components/signup/LoginCTA.tsx
+++ components/signup/LoginCTA.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 export const LoginCTA = () => (
-  <div className="text-center text-xl">
+  <div className="text-center text-xl py-8">
     Have an account?{' '}
     <Link href="/login">
       <a className="text-iflightblue">Log In</a>
