d344fe14a11a1fa9baebff6a834a0f8441088558
brekk <brekk@brekkbockrath.com>
2021-08-17 12:05:10 -1000

    IRO-886 - add preloader animation

--- components/signup/SignUpForm.tsx
+++ components/signup/SignUpForm.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import { RawButton } from '../Button'
 import LoginCTA from './LoginCTA'
 import Note from './Note'
@@ -5,6 +6,7 @@ import FinePrint from './FinePrint'
 import TextField from './TextField'
 import Select from './Select'
 import { Field } from '../../hooks/useForm'
+import Loader from '../Loader'
 
 interface SignUpFormProps {
   textFields: (Field | null)[]
@@ -16,24 +18,38 @@ export const SignUpForm = ({
   textFields,
   country,
   submit,
-}: SignUpFormProps) => (
-  <>
-    {textFields.map(t => t && <TextField key={t.id} {...t} />)}
-    {country && <Select {...country} />}
-    {country?.value === 'USA' && (
-      <Note>
-        <strong>Please note</strong>: US participants are not eligible for Iron
-        Fish coin rewards
-      </Note>
-    )}
-    <RawButton
-      className="w-full mt-8 max-w-md mb-2 text-lg md:text-xl p-3 md:py-5 md:px-4"
-      onClick={submit}
-    >
-      Sign Up
-    </RawButton>
-    <FinePrint />
-    <LoginCTA />
-  </>
-)
+}: SignUpFormProps) => {
+  const [$loaded, $setLoaded] = useState<boolean>(false)
+  useEffect(() => {
+    if (country?.value) {
+      $setLoaded(true)
+    }
+  }, [$loaded, $setLoaded, country])
+  return (
+    <>
+      {$loaded ? (
+        <>
+          {textFields.map(t => t && <TextField key={t.id} {...t} />)}
+          {country && <Select {...country} />}
+          {country?.value === 'USA' && (
+            <Note>
+              <strong>Please note</strong>: US participants are not eligible for
+              Iron Fish coin rewards
+            </Note>
+          )}
+          <RawButton
+            className="w-full mt-8 max-w-md mb-2 text-lg md:text-xl p-3 md:py-5 md:px-4"
+            onClick={submit}
+          >
+            Sign Up
+          </RawButton>
+          <FinePrint />
+          <LoginCTA />
+        </>
+      ) : (
+        <Loader />
+      )}
+    </>
+  )
+}
 export default SignUpForm
