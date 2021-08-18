9b2f555fa51ff2aff82ef9bf4bf3cadd21f8a36c
brekk <brekk@brekkbockrath.com>
2021-08-18 09:54:59 -1000

    setTouched on selection

--- components/signup/Select.tsx
+++ components/signup/Select.tsx
@@ -17,13 +17,20 @@ export const Select = ({
   valid,
   value,
   onChange,
+  setTouched,
 }: SelectField) => {
   const allOptions = noDefault
     ? [{ name: defaultLabel, value: defaultValue }].concat(options)
     : options
   return (
     <LabelledRow id={id} label={label} valid={valid} errorText={errorText}>
-      <select onChange={onChange} value={value}>
+      <select
+        onChange={e => {
+          onChange(e)
+          setTouched(true)
+        }}
+        value={value}
+      >
         {allOptions.map(({ value: option, name }: NameValue) => (
           <option key={option} value={option}>
             {name}
