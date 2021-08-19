a9563ce2d06bef5133d7192e01aeb84bcdc0612f
brekk <brekk@brekkbockrath.com>
2021-08-19 13:44:22 -1000

    test callback more

--- apiClient/client.ts
+++ apiClient/client.ts
@@ -89,3 +89,28 @@ export async function login(
     return new LocalError(e.message, 500)
   }
 }
+
+export async function tokenLogin(): Promise<ApiUser | ApiError | LocalError> {
+  /* eslint-disable-next-line no-console */
+  console.log({ magic, window })
+  if (typeof window === 'undefined' || !magic) {
+    return new LocalError('Only runnable in the browser', 500)
+  }
+  try {
+    const token = await magic.auth.loginWithCredential()
+    /* eslint-disable-next-line no-console */
+    console.log('TOKEN', token)
+    const res = await fetch(`${API_URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    /* eslint-disable-next-line no-console */
+    console.log('RES', res)
+    return res.json()
+  } catch (e) {
+    return new LocalError(e.message, 500)
+  }
+}
--- pages/callback.tsx
+++ pages/callback.tsx
@@ -1,32 +1,19 @@
 import { useEffect } from 'react'
 import Router from 'next/router'
-import Loader from '../components/Loader'
-import { magic } from '../utils/magic'
+import Loader from 'components/Loader'
+import { tokenLogin } from 'apiClient/client'
 
 const Callback = () => {
   useEffect(() => {
-    if (magic && 'auth' in magic) {
-      magic.auth.loginWithCredential().then(async (token: string | null) => {
-        try {
-          /* eslint-disable-next-line no-console */
-          console.log({ token })
-          const res = await fetch('api/login', {
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${token}`,
-            },
-          })
-          /* eslint-disable-next-line no-console */
-          console.log({ res })
-          if (res.status === 200) {
-            Router.push('/')
-            return
-          }
-        } catch (e) {
-          throw e
-        }
-      })
+    const call = async () => {
+      const res = await tokenLogin()
+      // eslint-disable-next-line no-console
+      console.log('TOKEN RES', res)
+      if (res && 'statusCode' in res && res.statusCode === 200) {
+        Router.push('/')
+      }
     }
+    call()
   }, [])
   return <Loader />
 }
