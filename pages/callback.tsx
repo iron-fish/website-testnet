c8b0cb2f754afb1dd2ef910d451e5e6689a46b90
brekk <brekk@brekkbockrath.com>
2021-08-19 13:26:31 -1000

    test callback more

--- pages/callback.tsx
+++ pages/callback.tsx
@@ -8,14 +8,19 @@ const Callback = () => {
     if (magic && 'auth' in magic) {
       magic.auth.loginWithCredential().then(async (token: string | null) => {
         try {
+          /* eslint-disable-next-line no-console */
+          console.log({ token })
           const res = await fetch('api/login', {
             headers: {
               'Content-Type': 'application/json',
               Authorization: `Bearer ${token}`,
             },
           })
+          /* eslint-disable-next-line no-console */
+          console.log({ res })
           if (res.status === 200) {
             Router.push('/')
+            return
           }
         } catch (e) {
           throw e
