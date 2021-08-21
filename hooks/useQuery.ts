2b10ec92fe77dc6fe083a89cee5669e75bff061f
brekk <brekk@brekkbockrath.com>
2021-08-20 14:43:16 -1000

    useState conventions

--- hooks/useQuery.ts
+++ hooks/useQuery.ts
@@ -1,15 +1,15 @@
 import { useEffect, useState } from 'react'
 
 export function useQuery(key: string): string | null {
-  const [$q, $setter] = useState<string | null>(null)
+  const [$query, $setQuery] = useState<string | null>(null)
   useEffect(() => {
     if (typeof window === 'undefined') return
     const parsed = new URLSearchParams((window.location.search || '').slice(1))
     const value = parsed.get(key)
     if (typeof value === 'string') {
-      $setter(value)
+      $setQuery(value)
     }
-  }, [$q, $setter, key])
-  return $q
+  }, [$query, $setQuery, key])
+  return $query
 }
 export default useQuery
