83901b007e858328edfb000fb13adf8671071039
brekk <brekk@brekkbockrath.com>
2021-08-24 11:15:35 -1000

    remove all props

--- components/OffsetBorder/Box.tsx
+++ components/OffsetBorder/Box.tsx
@@ -1,31 +1,12 @@
-import styles from './index.module.css'
 import { Props } from './types'
 
-export function Box({
-  style = {},
-  className = '',
-  backgroundClassName = '',
-  container = false,
-  children,
-  animated = false,
-}: Props) {
-  const classAnimated = animated ? styles.animated : ''
+export function Box({ container = false, children }: Props) {
   return (
-    <div
-      className={`relative mb-2.5 ${
-        container ? `ml-2.5` : ``
-      } mr-2.5 ${classAnimated}`}
-    >
+    <div className={`relative mb-2.5 ${container ? `ml-2.5` : ``} mr-2.5`}>
       <div
-        className={`absolute border border-black top-2.5 -bottom-2.5 left-2.5 -right-2.5 ${backgroundClassName} ${classAnimated}`}
-        style={style}
+        className={`absolute border border-black top-2.5 -bottom-2.5 left-2.5 -right-2.5`}
       />
-      <div
-        className={`relative border border-black bg-white ${className} ${classAnimated}`}
-        style={style}
-      >
-        {children}
-      </div>
+      <div className={`relative border border-black bg-white`}>{children}</div>
     </div>
   )
 }
--- components/OffsetBorder/types.ts
+++ components/OffsetBorder/types.ts
@@ -1,10 +1,4 @@
-import CSS from 'csstype'
-
 export type Props = {
   container?: boolean
-  style?: CSS.Properties
-  className?: string
-  backgroundClassName?: string
   children?: React.ReactNode
-  animated?: boolean
 }
