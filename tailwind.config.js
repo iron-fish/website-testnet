42d9000d7e12548f110dd94c42133ff4459d05b0
brekk <brekk@brekkbockrath.com>
2021-08-27 13:33:45 -1000

    unwork-some-unwork

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,9 @@ module.exports = {
         px3: '0.1875rem',
         4.5: '1.125rem',
       },
+      inset: {
+        5.5: '88px',
+      },
       minHeight: {
         section: '43rem',
       },
@@ -42,6 +45,9 @@ module.exports = {
         font: 'font-size',
         padding: 'padding',
         transform: 'transform',
+      },
+      transitionDuration: {
+        fast: '0.3s',
       },
       colors: {
         alertyellow: {
