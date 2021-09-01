3f1d0f3f45cbebfde171654be911efb6ba6c9cc1
Brekk <brekk@brekkbockrath.com>
2021-09-01 07:27:28 -1000

    IRO-897 - back to top button (#38)
    * IRO-897 - back to top button
    
    * hide at small breakpoints
    
    * bg-smalls

---
+++ components/icons/UpArrow.tsx
@@ -0,0 +1,17 @@
+export const UpArrow = () => (
+  <svg
+    width="24"
+    height="35"
+    viewBox="0 0 24 35"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12.25 34.5L12.25 3.25M1.75 18.25L12.25 2.75L22.75 18.25"
+      stroke="black"
+      strokeWidth="2"
+    />
+  </svg>
+)
+
+export default UpArrow
