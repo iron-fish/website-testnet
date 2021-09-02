cc2d5135525879e238d8c9f34a8bc06ea0b30cb0
brekk <brekk@brekkbockrath.com>
2021-09-02 08:47:43 -1000

    remove log

--- pages/login.tsx
+++ pages/login.tsx
@@ -63,8 +63,6 @@ export default function Login() {
       const email = $email?.value
 
       const result = await login(email)
-      // eslint-disable-next-line no-console
-      console.log({ result })
       if ('error' in result) {
         const error = '' + result.message
         $setLoaded(true)
