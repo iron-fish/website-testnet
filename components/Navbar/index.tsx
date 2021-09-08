dcea9ef94c0fd5380656d8c102bfb017d49c22e4
Brekk <brekk@brekkbockrath.com>
2021-09-07 19:02:45 -1000

    menu fixery (#45)

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import clsx from 'clsx'
 import Logo from 'components/Logo'
 import Menu from 'components/icons/Menu'
 import { useNav, NavState } from 'hooks/useNav'
@@ -54,13 +55,37 @@ function Navbar({
         closeFlyout={() => $setFlyoutVisible(false)}
         {...navBarLinksProps}
       />
-      <div className="flex items-stretch justify-between px-3 lg:px-10 2lg:px-16 max-w-menu m-auto">
+      <div
+        className={clsx([
+          'transition-width',
+          'delay-300',
+          'flex',
+          'items-stretch',
+          'justify-between',
+          'm-auto',
+          // md - 768px
+          'text-sm',
+          'px-5',
+          'md:max-w-[768px]',
+          // lg - 1024px
+          'lg:text-[18px]',
+          'lg:max-w-[1024px]',
+          // 2lg - 1152px
+          '2lg:px-10',
+          '2lg:text-xl',
+          '2lg:max-w-[1152px]',
+          // 1440
+          '1.5lg:px-16',
+          // 1700
+          '3xl:max-w-[1700px]',
+        ])}
+      >
         <div className="py-7">
           <Logo fill={fill} width={190} height={32}></Logo>
         </div>
         <div className="hidden md:flex items-center lg:text-xl">
           <NavbarLinks
-            className="px-1.5 lg:px-3 h-full flex items-center whitespace-nowrap transition-font transition-fast transition-padding"
+            className="h-full flex items-center whitespace-nowrap transition-font transition-fast transition-padding"
             {...navBarLinksProps}
           />
         </div>
