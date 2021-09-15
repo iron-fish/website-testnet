5f28a7da24a7d5b52a71f65f9b7c81eca4b64e22
Derek Guenther <dguenther9@gmail.com>
2021-09-15 12:51:23 -0700

    Use React Context for login state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,37 @@
 import Head from 'next/head'
 import Footer from 'components/Footer'
 import Navbar from 'components/Navbar'
-import useLogin from 'hooks/useLogin'
+import { LoginContext } from 'contexts/LoginContext'
 
 export default function Home() {
-  const { magicMetadata } = useLogin()
   return (
-    <div className="min-h-screen flex flex-col">
-      <Head>
-        <title>Iron Fish</title>
-        <meta
-          name="description"
-          content="Iron Fish Incentivized Testnet. Iron Fish is a new layer-1 blockchain and the future universal privacy layer for all things crypto."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <LoginContext.Consumer>
+      {({ magicMetadata }) => {
+        return (
+          <div className="min-h-screen flex flex-col">
+            <Head>
+              <title>Iron Fish</title>
+              <meta
+                name="description"
+                content="Iron Fish Incentivized Testnet. Iron Fish is a new layer-1 blockchain and the future universal privacy layer for all things crypto."
+              />
+              <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <Navbar fill="black" className="bg-iforange text-black" />
+            <Navbar fill="black" className="bg-iforange text-black" />
 
-      <main className="bg-iforange flex-1">
-        <h1 className="text-2xl">
-          {magicMetadata && magicMetadata.email
-            ? `Welcome to Testnet, ${magicMetadata.email}!`
-            : `Welcome to Testnet.`}
-        </h1>
-      </main>
+            <main className="bg-iforange flex-1">
+              <h1 className="text-2xl">
+                {magicMetadata && magicMetadata.email
+                  ? `Welcome to Testnet, ${magicMetadata.email}!`
+                  : `Welcome to Testnet.`}
+              </h1>
+            </main>
 
-      <Footer />
-    </div>
+            <Footer />
+          </div>
+        )
+      }}
+    </LoginContext.Consumer>
   )
 }
