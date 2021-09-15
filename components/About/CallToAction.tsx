4db6f38eefec1da769fba9715e4c1316d8239b58
Brekk <brekk@brekkbockrath.com>
2021-09-15 11:05:04 -0700

    IRO-1059 - font-size for titles (#54)
    * font-size for titles
    
    * single sizes

--- components/About/CallToAction.tsx
+++ components/About/CallToAction.tsx
@@ -37,8 +37,8 @@ export const CallToAction = ({
     <div className="mb-3">
       <Box behind="bg-ifpink">
         <div className="p-13">
-          <strong className="uppercase">{kind}</strong>
-          <h3 className="text-left text-2xl md:text-3xl my-5 font-extended">
+          <strong className="uppercase text-lg">{kind}</strong>
+          <h3 className="text-left text-4xl mt-3 mb-4 font-extended">
             {title}
           </h3>
           {children}
