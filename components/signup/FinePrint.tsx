19df6455b56e44c442e8bd97b36834c7e279c1f1
Derek Guenther <dguenther9@gmail.com>
2021-09-30 16:20:27 -0700

    IRO-1156 Sign Up page's Testnet Guidelines should link to page

--- components/signup/FinePrint.tsx
+++ components/signup/FinePrint.tsx
@@ -1,6 +1,11 @@
+import Link from 'next/link'
+
 export const FinePrint = () => (
   <span className="font-favorit p-2 max-w-md text-sm text-center">
-    By clicking on sign up, you agree to Iron Fish&apos;s Testnet Guidelines.
+    By clicking on sign up, you agree to Iron Fish&apos;s{' '}
+    <Link href="/about#guidelines">
+      <a className="text-iflightblue">Testnet Guidelines.</a>
+    </Link>
   </span>
 )
 export default FinePrint
