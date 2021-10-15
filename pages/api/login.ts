87781065c3d56ab3183954eb4f4ffa0ef2d26b70
brekk <brekk@brekkbockrath.com>
2021-10-15 09:52:27 -0700

    the killer is in the house

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -7,7 +7,6 @@ const magic = new Magic(process.env.NEXT_PUBLIC_MAGIC_SECRET_KEY || '')
 export default async function login(req: NextApiRequest, res: NextApiResponse) {
   try {
     // eslint-disable-next-line
-    console.log({ auth: req?.headers?.authorization })
     const authString = req?.headers?.authorization || ''
     if (!authString) {
       res.status(500).json({ error: 'No auth token provided' })
