b43d7ba7ce4d805181308b68017aa3e43e82d3a5
Elena Nadolinski <elena@ironfish.network>
2021-11-29 12:17:29 -0800

    Remove US warning on signup (#103)

--- components/signup/SignUpForm.tsx
+++ components/signup/SignUpForm.tsx
@@ -1,6 +1,5 @@
 import { RawButton } from 'components/Button'
 import TextField from 'components/Form/TextField'
-import Note from 'components/Form/Note'
 import Select from 'components/Form/Select'
 import LoginCTA from './LoginCTA'
 import FinePrint from './FinePrint'
@@ -21,12 +20,6 @@ export const SignUpForm = ({
     <>
       {textFields.map(t => t && <TextField key={t.id} {...t} />)}
       {country && <Select {...country} />}
-      {country?.value === 'USA' && (
-        <Note>
-          <strong>Please note</strong>: US participants are not eligible for
-          Iron Fish coin rewards
-        </Note>
-      )}
       <RawButton
         className="w-full mt-8 max-w-md mb-2 text-lg md:text-xl p-3 md:py-5 md:px-4"
         onClick={submit}
