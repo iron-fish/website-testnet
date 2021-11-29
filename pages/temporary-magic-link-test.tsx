1aa56e563fbf961e3ec51746337d09405cfe751a
brekk <brekk@brekkbockrath.com>
2021-11-29 12:21:01 -0800

    hellish symbiosis

--- pages/temporary-magic-link-test.tsx
+++ pages/temporary-magic-link-test.tsx
@@ -2,8 +2,8 @@ import { useProtectedRoute } from 'hooks/useProtectedRoute'
 import Debug from 'components/Debug'
 
 const Test = () => {
-  const { status } = useProtectedRoute({})
-  return <Debug {...{ status }} />
+  const deets = useProtectedRoute({})
+  return <Debug {...deets} />
 }
 
 export default Test
