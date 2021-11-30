9803e5a2ad9d37777049f6bd423ee87512c93cc0
Elena Nadolinski <elena@ironfish.network>
2021-11-30 13:07:17 -0800

    wip

--- components/Navbar/Testnet.tsx
+++ components/Navbar/Testnet.tsx
@@ -72,6 +72,14 @@ function Testnet({ condensed = false }: TestnetProps) {
                   textClassName={textClassName}
                   cubeClassName="text-ifcubepink"
                 />
+                <TestnetGridElement
+                  href="/faq"
+                  header="Testnet FAQ"
+                  body="Frequently asked questions"
+                  className={elementClassName}
+                  textClassName={textClassName}
+                  cubeClassName="text-iflightblue"
+                />
               </div>
             </div>
           </div>
