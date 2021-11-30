ae5852bec8d2cb53d49e21c92d72a09be9f6b9c2
brekk <brekk@brekkbockrath.com>
2021-11-29 16:56:19 -0800

    more tries

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,10 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+//
+Cypress.on('uncaught:exception', err => {
+  console.log(err)
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+})
