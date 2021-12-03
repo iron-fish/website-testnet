95c99e223cf0bb382fa16d80287a54b0a77b01aa
Derek Guenther <dguenther9@gmail.com>
2021-12-03 11:37:05 -0800

    Hide settings tab on non-authed user page

--- components/user/Tabs/SettingsContent.tsx
+++ components/user/Tabs/SettingsContent.tsx
@@ -7,40 +7,40 @@ import { useField } from 'hooks/useForm'
 import { FIELDS } from 'pages/signup'
 
 type Props = {
-  userMetadata: API.ApiUserMetadata
+  authedUser: API.ApiUserMetadata
 }
 
 const getSocialChoice = (
-  userMetaData: API.ApiUserMetadata
+  authedUser: API.ApiUserMetadata
 ): { choice: string; value: string } => {
-  if (userMetaData.discord) {
-    return { choice: 'discord', value: userMetaData.discord }
-  } else if (userMetaData.telegram) {
-    return { choice: 'telegram', value: userMetaData.telegram }
+  if (authedUser.discord) {
+    return { choice: 'discord', value: authedUser.discord }
+  } else if (authedUser.telegram) {
+    return { choice: 'telegram', value: authedUser.telegram }
   }
   return { choice: '', value: '' }
 }
 
-export default function SettingsContent({ userMetadata }: Props) {
+export default function SettingsContent({ authedUser }: Props) {
   const $email = useField(FIELDS.email)
   const $graffiti = useField(FIELDS.graffiti)
   const $social = useField(FIELDS.social)
   const $country = useField(FIELDS.country)
 
   const { choice: socialChoice, value: socialValue } =
-    getSocialChoice(userMetadata)
+    getSocialChoice(authedUser)
 
   return (
     <div className="flex">
       <div className="flex-initial">
         <div className="font-favorit mt-8">User Settings</div>
         {$email && (
-          <TextField {...$email} defaultValue={userMetadata.email} disabled />
+          <TextField {...$email} defaultValue={authedUser.email} disabled />
         )}
         {$graffiti && (
           <TextField
             {...$graffiti}
-            defaultValue={userMetadata.graffiti}
+            defaultValue={authedUser.graffiti}
             disabled
           />
         )}
@@ -53,7 +53,7 @@ export default function SettingsContent({ userMetadata }: Props) {
           />
         )}
         {$country && (
-          <Select {...$country} disabled value={userMetadata.country_code} />
+          <Select {...$country} disabled value={authedUser.country_code} />
         )}
       </div>
     </div>
