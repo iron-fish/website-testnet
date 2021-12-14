da37b387427a7d335d6a1c0da3a952adfc423a9e
Elena Nadolinski <elena@ironfish.network>
2021-12-13 17:11:19 -0800

    linkable faq section

--- components/FAQ/QuestionAnswer.tsx
+++ components/FAQ/QuestionAnswer.tsx
@@ -1,5 +1,6 @@
 import { Box } from 'components/OffsetBorder/Box'
 import { ReactNode } from 'react'
+import styles from './styles.module.css'
 
 type Props = {
   index: number
@@ -11,7 +12,17 @@ export default function QuestionAnswer({ index, question, answer }: Props) {
   return (
     <Box behind={index % 2 ? 'bg-iflightorange' : 'bg-ifpink'}>
       <div className="p-12">
-        <div className="font-extended text-[1.75rem] mb-4">{question}</div>
+        <div id={String(index)} className="font-extended text-[1.75rem] mb-4">
+          {question}{' '}
+          <a
+            className={styles.directLink}
+            aria-hidden="true"
+            href={'#' + index}
+            title="Direct link to question"
+          >
+            #
+          </a>{' '}
+        </div>
         <div className="font-favorit text-xl">{answer}</div>
       </div>
     </Box>
