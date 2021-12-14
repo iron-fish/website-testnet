4fe1a750ddb9edc4ce9157f01a5bb6ba888ec12e
Brekk <brekk@brekkbockrath.com>
2021-12-13 18:06:30 -0800

    add id literal values (#139)

--- components/FAQ/QuestionAnswer.tsx
+++ components/FAQ/QuestionAnswer.tsx
@@ -4,20 +4,21 @@ import styles from './styles.module.css'
 
 type Props = {
   index: number
+  id: string
   question: string
   answer: ReactNode
 }
 
-export default function QuestionAnswer({ index, question, answer }: Props) {
+export default function QuestionAnswer({ index, id, question, answer }: Props) {
   return (
     <Box behind={index % 2 ? 'bg-iflightorange' : 'bg-ifpink'}>
       <div className="p-12">
-        <div id={String(index)} className="font-extended text-[1.75rem] mb-4">
+        <div id={id} className="font-extended text-[1.75rem] mb-4">
           {question}{' '}
           <a
             className={styles.directLink}
             aria-hidden="true"
-            href={'#' + index}
+            href={'#' + id}
             title="Direct link to question"
           >
             #
