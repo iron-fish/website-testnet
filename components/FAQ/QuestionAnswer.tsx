b87b450d346f3d5dc98378c5e4b7386b32bca2da
Elena Nadolinski <elena@ironfish.network>
2021-12-13 18:15:55 -0800

    Linkable FAQ section (#138)
    * linkable faq section
    
    * add id literal values (#139)
    
    * adding fade
    
    Co-authored-by: Brekk <brekk@brekkbockrath.com>

--- components/FAQ/QuestionAnswer.tsx
+++ components/FAQ/QuestionAnswer.tsx
@@ -1,17 +1,29 @@
 import { Box } from 'components/OffsetBorder/Box'
 import { ReactNode } from 'react'
+import styles from './styles.module.css'
 
 type Props = {
   index: number
+  id: string
   question: string
   answer: ReactNode
 }
 
-export default function QuestionAnswer({ index, question, answer }: Props) {
+export default function QuestionAnswer({ index, id, question, answer }: Props) {
   return (
     <Box behind={index % 2 ? 'bg-iflightorange' : 'bg-ifpink'}>
       <div className="p-12">
-        <div className="font-extended text-[1.75rem] mb-4">{question}</div>
+        <div id={id} className="font-extended text-[1.75rem] mb-4">
+          {question}{' '}
+          <a
+            className={styles.directLink}
+            aria-hidden="true"
+            href={'#' + id}
+            title="Direct link to question"
+          >
+            #
+          </a>{' '}
+        </div>
         <div className="font-favorit text-xl">{answer}</div>
       </div>
     </Box>
