dc9ad3b85e9bf6c1f34f08d354ac8fe6a6082e72
brekk <brekk@brekkbockrath.com>
2022-03-08 10:13:24 -0800

    add some logic

--- pages/me.tsx
+++ pages/me.tsx
@@ -1,7 +1,16 @@
+import { useRouter } from 'next/router'
 import { Props, User } from './users/[id]'
+import { encode as btoa } from 'base-64'
 
 export function Me({ loginContext }: Props) {
-  return <User loginContext={loginContext} startTab="settings" />
+  const $router = useRouter()
+  if (loginContext?.metadata?.id) {
+    return <User loginContext={loginContext} startTab="settings" />
+  } else {
+    return $router.push(
+      `/login?toast=${btoa('You must be logged in to see that page.')}`
+    )
+  }
 }
 
 export default Me
