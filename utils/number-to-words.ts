e200a0d367c5015af686b5ba86b123b7ee5ca28f
brekk <brekk@brekkbockrath.com>
2022-05-11 17:34:31 -0700

    access

--- utils/number-to-words.ts
+++ utils/number-to-words.ts
@@ -1,29 +1,16 @@
 export const digit = (x: number): string => {
-  if (x === 9) {
-    return 'nine'
-  }
-  if (x === 8) {
-    return 'eight'
-  }
-  if (x === 7) {
-    return 'seven'
-  }
-  if (x === 6) {
-    return 'six'
-  }
-  if (x === 5) {
-    return 'five'
-  }
-  if (x === 4) {
-    return 'four'
-  }
-  if (x === 3) {
-    return 'three'
-  }
-  if (x === 2) {
-    return 'two'
-  }
-  return 'one'
+  return [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ][x]
 }
 
 export const capitalDigit = (x: number) => {
