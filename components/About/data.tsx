a41afcb04356db6b7c9e542672f6d3f26ddf549a
Joseph Parks <jowparks@gmail.com>
2022-11-16 14:49:55 -0800

    chore: update masp cards

--- components/About/data.tsx
+++ components/About/data.tsx
@@ -128,23 +128,6 @@ export const callsToAction = {
       href: 'https://forms.gle/agquNGnS5NDgcfZN6',
       status: Status.Active,
     },
-  ],
-  columnTwo: [
-    {
-      title: `Sending Multi Asset Transactions`,
-      content: (
-        <>
-          Once a week, you can submit a mint, burn, and transfer of an a custom
-          multi asset, where the asset name is your graffiti:
-        </>
-      ),
-      points: [
-        'X points for multi asset mint',
-        'X points for multi asset burn',
-        'X points for multi asset transfer',
-      ],
-      status: Status.New,
-    },
     {
       title: 'Finding Bugs',
       content: (
@@ -165,6 +148,41 @@ export const callsToAction = {
       href: 'https://forms.gle/agquNGnS5NDgcfZN6',
       submissionForm: false,
       status: Status.Active,
+    },
+  ],
+  columnTwo: [
+    {
+      title: `Mint Asset`,
+      content: (
+        <>
+          Once a week, you can submit a mint your asset, where the asset name is
+          your graffiti:
+        </>
+      ),
+      points: ['X points per transaction', 'Weekly limit = 1'],
+      status: Status.New,
+    },
+    {
+      title: `Burn Asset`,
+      content: (
+        <>
+          Once a week, you can submit a burn your asset, where the asset name is
+          your graffiti:
+        </>
+      ),
+      points: ['X points per transaction', 'Weekly limit = 1'],
+      status: Status.New,
+    },
+    {
+      title: `Send Asset`,
+      content: (
+        <>
+          Once a week, you can submit a send your asset, where the asset name is
+          your graffiti:
+        </>
+      ),
+      points: ['X points per transaction', 'Weekly limit = 1'],
+      status: Status.New,
     },
   ],
 }
