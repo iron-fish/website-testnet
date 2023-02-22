6cffb89f8dc3343e5289d33cda4f931d52f2fc68
Daniel Cortes <danielgcortes@gmail.com>
2023-02-22 17:38:23 -0600

    InfoChip colors

--- components/Airdrop/InfoChip/InfoChip.tsx
+++ components/Airdrop/InfoChip/InfoChip.tsx
@@ -1,34 +1,45 @@
 import clsx from 'clsx'
 
 import { ReactNode, useMemo } from 'react'
+import PendingIcon from './icons/Pending'
+import WarningIcon from './icons/Warning'
+
+import styles from './InfoChip.module.css'
 
 type ChipProps = {
   children: ReactNode
-  variant: 'info' | 'pending' | 'warning'
+  variant: 'info' | 'pending' | 'warning' | 'complete'
 }
 
 export function InfoChip({ children, variant }: ChipProps) {
   const colors = useMemo(() => {
     return {
-      info: ['bg-iflightgray'],
-      pending: ['bg-iflightbeige'],
-      warning: ['text-alertred', 'bg-alertred', 'bg-opacity-10'],
+      info: styles.info,
+      pending: styles.pending,
+      warning: styles.warning,
+      complete: styles.complete,
     }[variant]
   }, [variant])
   return (
     <div
       className={clsx(
         'text-sm',
-        'text-center',
         'md:text-md',
         'px-3',
         'py-2',
-        ...colors,
         'inline-block',
-        'rounded'
+        'rounded',
+        'inline-flex',
+        'items-center',
+        'gap-2',
+        'justify-center',
+        colors
       )}
     >
-      {children}
+      {variant === 'warning' && <WarningIcon />}
+      {variant === 'pending' && <PendingIcon />}
+      {variant === 'complete' && <WarningIcon />}
+      <div>{children}</div>
     </div>
   )
 }
