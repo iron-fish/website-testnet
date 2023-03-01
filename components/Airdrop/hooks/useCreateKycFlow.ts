ca6e80b0be843fd77da01ccb35d43e24ead89b03
Daniel Cortes <danielgcortes@gmail.com>
2023-03-01 16:09:33 -0700

    Add content-type

--- components/Airdrop/hooks/useCreateKycFlow.ts
+++ components/Airdrop/hooks/useCreateKycFlow.ts
@@ -18,6 +18,7 @@ export function useCreateKycFlow(userAddress: string) {
         const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/kyc`, {
           method: 'POST',
           headers: {
+            'content-type': 'application/json',
             Authorization: `Bearer ${apiKey}`,
           },
           body: JSON.stringify({
