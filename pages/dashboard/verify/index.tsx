ba5954737e742ba6da201589d761fa99391d3868
Daniel Cortes <danielgcortes@gmail.com>
2023-02-28 21:54:23 -0700

    Fix types

--- pages/dashboard/verify/index.tsx
+++ pages/dashboard/verify/index.tsx
@@ -6,7 +6,7 @@ import Footer from 'components/Footer'
 import Navbar from 'components/Navbar'
 
 import Loader from 'components/Loader'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { JumioIframe } from 'components/Airdrop/JumioIframe/JumioIframe'
 import StepSubmitAddress from 'components/Airdrop/StepSubmitAddress/StepSubmitAddress'
 import StepConfirmJumioStart from 'components/Airdrop/StepConfirmJumioStart/StepConfirmJumioStart'
@@ -14,55 +14,6 @@ import StepConfirmJumioStart from 'components/Airdrop/StepConfirmJumioStart/Step
 type AboutProps = {
   showNotification: boolean
   loginContext: LoginContext
-}
-
-function useWhoAmI() {
-  const [myData, setMyData] = useState(null)
-
-  useEffect(() => {
-    const doFetch = async () => {
-      const res = await fetch('/api/whoami')
-      const json = await res.json()
-      setMyData(json)
-    }
-
-    doFetch()
-  }, [])
-
-  return myData
-}
-
-function useJumioTransaction({ userId }: { userId?: number }): null | {
-  redirectUrl: string
-  timestamp: string
-  transactionReference: string
-} {
-  const [jumioTransaction, setJumioTransaction] = useState(null)
-
-  const _data = useWhoAmI()
-  // eslint-disable-next-line no-console
-  console.log(_data)
-
-  useEffect(() => {
-    if (!userId) {
-      return
-    }
-
-    const doFetch = async () => {
-      const res = await fetch('/api/jumio', {
-        method: 'POST',
-        body: JSON.stringify({
-          userId,
-        }),
-      })
-      const json = await res.json()
-      setJumioTransaction(json)
-    }
-
-    doFetch()
-  }, [userId])
-
-  return jumioTransaction
 }
 
 export default function Verify({ showNotification, loginContext }: AboutProps) {
@@ -73,15 +24,13 @@ export default function Verify({ showNotification, loginContext }: AboutProps) {
   const _loggedIn = checkLoggedIn()
   const _userId = metadata?.id
 
-  // const jumioData = useJumioTransaction({ userId })
-
   return checkLoading() ? (
     <Loader />
   ) : (
     <div className={clsx('min-h-screen', 'flex', 'flex-col', 'font-favorit')}>
       <Head>
-        <title>KYC TEMP TITLE</title>
-        <meta name="description" content="KYC TEMP DESCRIPTION" />
+        <title>KYC Form</title>
+        <meta name="description" content="" />
       </Head>
       <Navbar
         showNotification={showNotification}
