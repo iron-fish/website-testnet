e7fc41d3663b131acb94e99df5d63c847e7a855b
Jason Spafford <nullprogrammer@gmail.com>
2023-03-18 16:24:26 -0400

    Simplified down KYC copy

--- components/Airdrop/KYCSteps/StepConfirmJumioStart.tsx
+++ components/Airdrop/KYCSteps/StepConfirmJumioStart.tsx
@@ -12,15 +12,22 @@ export default function StepConfirmJumioStart({ onNext }: Props) {
       <div className={clsx('p-12', 'flex', 'flex-col')}>
         <h1 className={clsx('font-extended', 'text-4xl', 'mb-8')}>KYC Form</h1>
         <p className="mb-2">
-          You are about to begin the identity verification process. In order to
-          successfully complete this process, you will be asked to provide a
-          valid government-issued ID. Please ensure you have a valid ID ready.
+          KYC will require you to upload a passport, driver&#39;s license or
+          government identity card.
         </p>
-        <p className="mb-2">
-          Once started, you will have 15 minutes to finish. If you do not finish
-          in time, the KYC process will fail.
+        <p className="mb-2">You will have 15 minutes to complete KYC.</p>
+        <p>
+          Click here to{' '}
+          <a
+            className="underline"
+            target="_blank"
+            href="https://coda.io/d/_dte_X_jrtqj/KYC-FAQ_su_vf#_luc1r"
+            rel="noreferrer"
+          >
+            see our KYC FAQ
+          </a>
+          .
         </p>
-        <p>You will have up to 3 attempts to complete the KYC process.</p>
         <div className="mb-auto" />
         <div className={clsx('flex', 'justify-end')}>
           <Button
