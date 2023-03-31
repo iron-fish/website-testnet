7f5ca62d3067314f1ce3940e24e51289c212e4ab
ygao76 <yajun@ironfish.network>
2023-03-31 16:55:36 -0700

    check response

--- pages/callback.tsx
+++ pages/callback.tsx
@@ -32,7 +32,7 @@ const Callback = () => {
         }
         const details = await getUserDetails(token)
 
-        if ('statusCode' in details && details.statusCode === 200) {
+        if ('email' in details && details.email.length > 0 ) {
           $router.push(`/dashboard?toast=${btoa('Welcome back!')}`)
           return
         }
