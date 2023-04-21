9ea437aa4a4a839c0c735123bdaa3a9aef37afa6
Joe <joe@iflabs.network>
2023-04-21 09:30:41 -0700

    feat: disable edit button for public address

--- components/Airdrop/WalletAddress/WalletAddress.tsx
+++ components/Airdrop/WalletAddress/WalletAddress.tsx
@@ -86,12 +86,6 @@ export default function WalletAddress({ address }: { address: string }) {
             'md:items-center'
           )}
         >
-          <button
-            onClick={() => setIsEditing(true)}
-            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto hover:bg-gray-50"
-          >
-            Edit
-          </button>
           <div
             className={clsx(
               'py-2',
